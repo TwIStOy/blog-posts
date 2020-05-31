ac417fb7bac6f9e18b93c633b31582a01d346d97
Hawtian Wang <twistoy.wang@gmail.com>
2020-06-01 04:16:40 +0800

    :wrench: add deploy script

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
+fi
+
+main() {
+  git submodule update --init --recursive
+
+  version=$(zola --version)
+  echo "Using $version"
+
+  zola build
+
+  cd public
+
+  git init
+  git config user.name "GitHub Actions"
+  git config user.email "github-actions-bot@users.noreply.github.com"
+  git add .
+
+  remote_repo="https://${GITHUB_TOKEN}@github.com/${PAGES_REPOSITORY}.git"
+  remote_branch=$PAGES_BRANCH
+
+  git push --force "${remote_repo}" master:${remote_branch}
+}
+
+main "$@"
